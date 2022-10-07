29a39d727e5c112021f6ad92c1e26a98ea560698
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2022-10-07 14:37:38 +0000

    Added image. Finished upload page

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,7 +1,7 @@
 import { faCloudArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import CircleBlobElement from "../../utils/circleBlobElement";
 import { importAllFilesFromFolder } from "../../utils/utils";
+import uploadImg from "../../assets/upload/upload.png"
 
 const UploadPage = () => {
     const listInstitutionImages: any = importAllFilesFromFolder(
@@ -10,35 +10,41 @@ const UploadPage = () => {
 
     return (
         <div className="mt-4 mx-auto w-75 px-5">
-            <h4>
-                <span
-                    className="text-secondary rounded p-1"
-                    style={{ backgroundColor: "rgba(214,95,48,0.2)" }}
-                >
-                    <FontAwesomeIcon icon={faCloudArrowUp} />
-                </span>{" "}
+            <h5 className="d-flex align-items-center text-secondary fw-bold">
+                <FontAwesomeIcon
+                    icon={faCloudArrowUp}
+                    pull="left"
+                    style={{
+                        width: "30px",
+                        height: "30px",
+                        backgroundColor: "rgba(214,95,48,0.2)",
+                        padding: "8px",
+                    }}
+                    className="me-3 fs-4 rounded"
+                />
                 How to Submit Data
-            </h4>
-            <hr />
+            </h5>
+            <hr className="border-secondary mb-4" />
             <p>
                 The GHGA Data Portal is currently being developed, and will allow data
                 upload request over the portal in the future. Currently we are running a
                 pilot project named GHGA Metadata Catalog, working with partner
                 institutions to collect dataset. If you are located at a GHGA Partner
                 Institute and would like to upload your metadata to GHGA Metadata
-                Catalog, please the GHGA Helpdesk at <strong>dac-ghga@ghga.de</strong>.
+                Catalog, please contact the GHGA Helpdesk at <strong>ghga-office[at]dkfz-heidelberg[dot]de</strong>.
             </p>
             <p>
                 We are collecting the anonymous dataset metadata following the GHGA
-                Metadata Model , and keep it interoperable with the EGA Metadata Model.{" "}
+                Metadata Model, and keep it interoperable with the EGA Metadata Model.{" "}
             </p>
-            <CircleBlobElement colour="#404F65" diameter={125} text="Submission" borderSize={12} rotation={-35}/>
-            Submission Request Evaluation Process Submission
-            <p>
+            <div className="text-center">
+                <img src={uploadImg} alt="upload" className="mb-5" width="80%" />
+            </div>
+            <p className="mb-4">
                 As a research network, GHGA has multiple cooperations with national and
                 international partners. Some of our partners:
             </p>
-            <div className="d-flex justify-content-between w-100 flex-wrap">
+            <div className="d-flex justify-content-between w-100 flex-wrap mb-5">
                 {listInstitutionImages.map((x: any) => (
                     <div key={x}>
                         <img src={x} alt="Institution" className="me-4 my-2" />
