80f6307e9691615dc91810c8743566437a1dd454
Jordy <31940280+ac-jorellanaf@users.noreply.github.com>
2022-10-07 19:04:05 +0200

    Upload page
    - Upload page completed
    - Removed forcing of npm install, working without issues
    - closes GDEV-1296

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import FooterIcons from "./footerIcons";
 import FooterNavbar from "./footerNavbar";
+import {Buffer} from 'buffer';
 
 const Footer = () => {
   const svgCode: string =
     '<svg width="1440" height="120" preserveAspectRatio="none" viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M1482 66.8182H1119.93C854.272 66.8182 592.376 0 364.5 0C136.624 0 -8 66.8182 -8 66.8182V120H1482V66.8182Z" fill="#fff"/></svg>';
-  const svgEncoded: string = btoa(svgCode);
+  const svgEncoded: string = Buffer.from(svgCode).toString('base64');
   return (
     <footer>
       <FooterNavbar />
